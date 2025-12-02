be473976e2cd266504a203d7b8843811ccf80b91
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2025-12-02 15:42:46 -0400

    feat: Eliminar el plugin de Tailwind CSS de la configuración de Vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
@@ -27,6 +26,5 @@ export default defineConfig({
             ],
             refresh: true,
         }),
-        tailwindcss(),
     ],
 });
